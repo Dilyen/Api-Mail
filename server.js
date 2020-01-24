b8a07405b2e53b4243e7b0d46c174b28b035497e
Dilyen <davidandohacquah@gmail.com>
2020-01-24 13:57:27 +0000

    removed log

--- server.js
+++ server.js
@@ -13,7 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/Api-Mail/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, () => {
-consol.log('Server is listening on port 8080...............');
-
-});
+app.listen(process.env.PORT || 8080);
